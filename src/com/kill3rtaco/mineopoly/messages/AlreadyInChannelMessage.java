c230d6352d3b301470c69157d9aa208b4263c392
crafteverywhere <1196377271@qq.com>
2013-08-08 00:07:56 +0800

    Update AlreadyInChannelMessage.java

--- src/com/kill3rtaco/mineopoly/messages/AlreadyInChannelMessage.java
+++ src/com/kill3rtaco/mineopoly/messages/AlreadyInChannelMessage.java
@@ -5,7 +5,7 @@ import com.kill3rtaco.tacoapi.api.TacoMessage;
 public class AlreadyInChannelMessage extends TacoMessage{
 	
 	public AlreadyInChannelMessage(){
-		this.message = "&cYou are already listening to the &6MineopolyChannel";
+		this.message = "&c你已经收听&6Mineopoly频道";
 	}
 	
 }
