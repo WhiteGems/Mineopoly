e2c227a393d21d4aef909bea14d43a32bd3c780e
crafteverywhere <1196377271@qq.com>
2013-08-07 23:59:51 +0800

    Localization NotInJailMessage

--- src/com/kill3rtaco/mineopoly/messages/NotInJailMessage.java
+++ src/com/kill3rtaco/mineopoly/messages/NotInJailMessage.java
@@ -5,7 +5,7 @@ import com.kill3rtaco.tacoapi.api.TacoMessage;
 public class NotInJailMessage extends TacoMessage {
 
 	public NotInJailMessage(){
-		this.message = "&cYou are not in jail";
+		this.message = "&c你不在监狱中";
 	}
 	
 }
