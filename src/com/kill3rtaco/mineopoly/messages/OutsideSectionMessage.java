13cabc219e408ab292e75a9322ae46267d4f7654
crafteverywhere <1196377271@qq.com>
2013-08-08 00:04:39 +0800

    Localization OutsideSectionMessage

--- src/com/kill3rtaco/mineopoly/messages/OutsideSectionMessage.java
+++ src/com/kill3rtaco/mineopoly/messages/OutsideSectionMessage.java
@@ -5,7 +5,7 @@ import com.kill3rtaco.tacoapi.api.TacoMessage;
 public class OutsideSectionMessage extends TacoMessage {
 
 	public OutsideSectionMessage(){
-		this.message = "&cPlease stay on your space";
+		this.message = "&c请不要离开属于你自己的地方";
 	}
 	
 }
