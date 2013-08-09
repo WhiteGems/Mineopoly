a96e132f3d46ff8557cb4a2a6afaf31c3a2205bd
evileyecc <c490@hotmail.com>
2013-08-10 00:05:25 +0800

    Update MineopolyBanCommand.java

--- src/com/kill3rtaco/mineopoly/cmds/mineopoly/MineopolyBanCommand.java
+++ src/com/kill3rtaco/mineopoly/cmds/mineopoly/MineopolyBanCommand.java
@@ -10,7 +10,7 @@ import com.kill3rtaco.tacoapi.api.TacoCommand;
 public class MineopolyBanCommand extends TacoCommand {
 
 	public MineopolyBanCommand() {
-		super("ban", new String[]{}, "<player>", "Ban a player from playing Mineopoly", MineopolyConstants.P_BAN_PLAYER_FROM_GAME);
+		super("ban", new String[]{}, "<玩家ID>", "将一个玩家封禁, 使他不可以玩Mineopoly", MineopolyConstants.P_BAN_PLAYER_FROM_GAME);
 	}
 
 	@Override
@@ -25,11 +25,11 @@ public class MineopolyBanCommand extends TacoCommand {
 				Mineopoly.plugin.banPlayer(name);
 				if(p != null && p.isOnline() && Mineopoly.plugin.getGame().hasPlayer(p)){
 					MineopolyPlayer mp = Mineopoly.plugin.getGame().getBoard().getPlayer(p);
-					Mineopoly.plugin.getGame().kick(mp, "banned by " + player.getName());
+					Mineopoly.plugin.getGame().kick(mp, "封禁由" + player.getName());
 				}
-				Mineopoly.plugin.chat.sendPlayerMessage(player, "&ePlayer banned");
+				Mineopoly.plugin.chat.sendPlayerMessage(player, "&e玩家已被封禁");
 			}else{
-				Mineopoly.plugin.chat.sendPlayerMessage(player, "&cPlayer &e" + name + " &cis already banned");
+				Mineopoly.plugin.chat.sendPlayerMessage(player, "&c玩家 &e" + name + " &c已经被封禁");
 			}
 		}
 	}
@@ -37,7 +37,7 @@ public class MineopolyBanCommand extends TacoCommand {
 	@Override
 	public boolean onConsoleCommand(String[] args) {
 		if(args.length == 0){
-			Mineopoly.plugin.chat.out("Must specify a player to ban");
+			Mineopoly.plugin.chat.out("必须指定一个玩家进行封禁");
 		}else{
 			String name = args[0];
 			Player p = Mineopoly.plugin.getServer().getPlayer(args[0]);
@@ -46,11 +46,11 @@ public class MineopolyBanCommand extends TacoCommand {
 				Mineopoly.plugin.banPlayer(name);
 				if(p != null && p.isOnline() && Mineopoly.plugin.getGame().hasPlayer(p)){
 					MineopolyPlayer mp = Mineopoly.plugin.getGame().getBoard().getPlayer(p);
-					Mineopoly.plugin.getGame().kick(mp, "banned by CONSOLE");
+					Mineopoly.plugin.getGame().kick(mp, "由后台封禁");
 				}
-				Mineopoly.plugin.chat.out("&ePlayer banned");
+				Mineopoly.plugin.chat.out("&e玩家已被封禁");
 			}else{
-				Mineopoly.plugin.chat.out("&cPlayer &e" + name + " &cis already banned");
+				Mineopoly.plugin.chat.out("&c玩家 &e" + name + " &c已经被封禁");
 			}
 		}
 		return false;
